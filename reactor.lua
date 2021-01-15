daf0e6ff5b91cb0c5dc382df9f3ffae0b421e37c
Cubeson <30746869+Cubeson@users.noreply.github.com>
2021-01-15 19:49:51 +0100

    Update reactor.lua
    RF/t

--- reactor.lua
+++ reactor.lua
@@ -3,15 +3,23 @@ local monitor = peripheral.wrap("top")
 monitor.clear()
 
 while(1) do
+  -- RF stored
   monitor.setCursorPos(1, 1)
   currentEnergy = reactor.getEnergyStored()
   monitor.write(tostring(currentEnergy))
   monitor.write(" RF")
   
+  -- Fuel
   monitor.setCursorPos(1, 2)
   currentFuel = reactor.getFuelAmount()
   monitor.write(tostring(currentFuel))
   monitor.write(" Fuel")
+  
+  -- RF/t
+  monitor.setCursorPos(1, 3)
+  RFgeneration = reactor.getEnergyProducedLastTick()
+  monitor.write(tostring(RFgeneration))
+  monitor.write(" RF/t")
   
   sleep(0.5)
   monitor.clear()
